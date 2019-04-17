b9e8509d054a08257add175a2338f1a839505d88
Dominik Lindner <d.lindner@dundee.ac.uk>
2019-04-17 14:35:44 +0100

    Add install.R and environment.yml

---
+++ install.R
@@ -0,0 +1,6 @@
+install.packages("shinydashboard")
+install.packages("rJava")
+install.packages("httr")
+install.packages("jpeg")
+install.packages("https://github.com/ome/rOMERO-gateway/releases/download/v0.4.4/romero.gateway_0.4.4.tar.gz", repos = NULL)
+
